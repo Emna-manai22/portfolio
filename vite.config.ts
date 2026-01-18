4562eaabdddbb2edc6d5e202dc4cb7d40baaa07c
Emna-manai22 <emnamannai244@gmail.com>
2026-01-18 19:26:54 +0100

    updatev

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  root: ".",
-  plugins: [
-    react(),
-    tailwindcss(), // si tu veux utiliser Tailwind
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"), // alias pour src
-    },
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // default Vite build folder
   },
-  publicDir: "public",
 });
