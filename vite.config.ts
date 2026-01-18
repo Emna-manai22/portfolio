caed4e0bf5de7af48959e967151f5283dc759c9b
Emna-manai22 <emnamannai244@gmail.com>
2026-01-18 19:36:28 +0100

    fix: add alias @ for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // <-- ajoute cet alias pour @
+    },
+  },
   build: {
-    outDir: "dist", // default Vite build folder
+    outDir: "dist", // dossier de build par défaut
   },
 });
